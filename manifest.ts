ee91073784551e8ca0342befa7eae12aa22b511c
Wystov <wystov@gmail.com>
2024-01-09 15:06:00 +0100

    feat: app can be installed as pwa

---
+++ manifest.ts
@@ -0,0 +1,43 @@
+import { VitePWAOptions } from 'vite-plugin-pwa';
+
+export const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touc-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: 'Water sort',
+    short_name: 'Water sort',
+    description: 'Sort the water',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+      {
+        src: '/maskable_icon.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 import react from '@vitejs/plugin-react';
 
+import { manifestForPlugIn } from './manifest';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
