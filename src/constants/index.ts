19cf74be5ae55e8d33f7b00b37a71b916d31eaa8
Wystov <wystov@gmail.com>
2024-02-04 04:49:02 +0100

    feat: add more colors

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,8 +5,20 @@ export const COLORS = [
   '#a786df',
   '#ffa8ba',
   '#6246ea',
-  '#078080',
-  '#00214d',
+  '#c759a7',
+  '#009688',
+  '#5ed9bd',
+  '#4183d7',
+  '#c2958c',
+  '#6eb67b',
+  '#ed7c59',
+  '#9848be',
+  '#4d3b86',
+  '#c0d98a',
+  '#b7321f',
+  '#8db8d1',
+  '#e5d727',
+  '#264653',
 ] as const;
 
 export const PERK_COST = {
