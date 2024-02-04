406051d02bb60944354f92143fcc32081dc1bdf5
Wystov <wystov@gmail.com>
2024-02-04 01:10:27 +0100

    feat: shuffle initial bottles until all mixed

--- src/utils/createLvlData.ts
+++ src/utils/createLvlData.ts
@@ -1,6 +1,7 @@
 import { COLORS } from '@/constants';
 import { BottleType } from '@/types';
 
+import { isBottleSorted } from './isSolved';
 import { shuffleArray } from './shuffleArray';
 
 export const createLvlData = (
@@ -8,13 +9,20 @@ export const createLvlData = (
   bottlesCount: number,
   colorsCount: number
 ) => {
-  const fillList = [...COLORS]
-    .sort(() => Math.random() - 0.5)
-    .slice(0, colorsCount)
-    .flatMap((color) => Array(bottleParts).fill(color));
-  const shuffledArray = shuffleArray(fillList);
-  const bottles: BottleType[] = Array(bottlesCount)
-    .fill(null)
-    .map((_, i) => shuffledArray.slice(i * bottleParts, (i + 1) * bottleParts));
+  let bottles: BottleType[];
+
+  do {
+    const fillList = [...COLORS]
+      .sort(() => Math.random() - 0.5)
+      .slice(0, colorsCount)
+      .flatMap((color) => Array(bottleParts).fill(color));
+    const shuffledArray = shuffleArray(fillList);
+    bottles = Array(bottlesCount)
+      .fill(null)
+      .map((_, i) =>
+        shuffledArray.slice(i * bottleParts, (i + 1) * bottleParts)
+      );
+  } while (bottles.some(isBottleSorted));
+
   return bottles;
 };
